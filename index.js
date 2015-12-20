a9a7028572f0ba6a7db7a58d62b98d2c088f5e52
leeching <417300981@qq.com>
2015-12-20 22:54:16 +0800

    Add src directory

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const path = require('path')
 const koa = require('koa')
 const mount = require('koa-mount')
+const send = require('koa-send')
+const staticDir = require('koa-static')
+const route = require('koa-route')
 const graphqlHTTP = require('koa-graphql')
 const articleSchema = require('./schema/index')
 
 const PORT = 4000
 const app = koa()
 
+app.use(route.get('/', function *() {
+  yield send(this, 'public/index.html')
+}))
+app.use(mount('/assets', staticDir(path.join(__dirname, 'build'))))
 app.use(mount('/graphql', graphqlHTTP({
   schema: articleSchema,
   pretty: true,
   graphiql: true
 })))
-
 app.listen(PORT, () => console.log(`koa is listening on ${PORT}`))
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    vendor: [
+      'react',
+      'react-dom'
+    ],
+    app: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.js'
+    })
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {warning: false}
+    })
+  )
+} else {
+  module.exports.devtool = '#source-map'
+}
